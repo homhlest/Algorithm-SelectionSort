9196f8c678ca83eb3c45373ecff36bd11d6b0976
Waminms <67516571+Waminms@users.noreply.github.com>
2023-04-20 12:35:59 +0300

    Decomposition and comments improvements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,36 +2,13 @@
 import java.util.Arrays;
 
 /**
- * This program is a realization of selection sort algorithm.
- * This algorithm has an O(n2) time complexity, which makes it inefficient
- * on large lists, and generally performs worse than the similar insertion sort.
- * Selection sort is noted for its simplicity and has performance advantages
- * over more complicated algorithms in certain situations,
- * particularly where auxiliary memory is limited.
+ * This program is a realization of the selection sort algorithm.
  */
 public class Main {
     public static void main(String[] args) {
         int[] arr = new int[]{64, 25, -2, 22, 11, 0};
-
         System.out.println("Initial array: \n" + Arrays.toString(arr));
-        int indexOfMinElement = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            int sortedPartBorder = i;
-            int min = arr[sortedPartBorder];
-
-            while ((sortedPartBorder) < arr.length) { // searching the minimal element in the array
-                if (arr[sortedPartBorder] <= min) {
-                    min = arr[sortedPartBorder];
-                    indexOfMinElement = sortedPartBorder;
-                }
-                sortedPartBorder++;
-            }
-            int temp = arr[i];
-            arr[i] = min;
-            arr[indexOfMinElement] = temp;
-        }
-        System.out.println("Selection sort algorithm result: \n" + Arrays.toString(arr));
+        SelectionSortAlgorithm.sort(arr);
     }
 }
 
---
+++ src/main/java/SelectionSortAlgorithm.java
@@ -0,0 +1,33 @@
+
+import java.util.Arrays;
+
+/**
+ * This algorithm has an O(n2) time complexity, which makes it inefficient
+ * on large lists, and generally performs worse than the similar insertion sort.
+ * Selection sort is noted for its simplicity and has performance advantages
+ * over more complicated algorithms in certain situations, particularly where
+ * auxiliary memory is limited.
+ */
+public class SelectionSortAlgorithm {
+
+    public static void sort(int[] arr) {
+        int indexOfMinElement = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sortedPartBorder = i;
+            int min = arr[sortedPartBorder];
+
+            while ((sortedPartBorder) < arr.length) { // searching the minimal element in the array
+                if (arr[sortedPartBorder] <= min) {
+                    min = arr[sortedPartBorder];
+                    indexOfMinElement = sortedPartBorder;
+                }
+                sortedPartBorder++;
+            }
+            int temp = arr[i];
+            arr[i] = min;
+            arr[indexOfMinElement] = temp;
+        }
+        System.out.println("Selection sort algorithm result: \n" + Arrays.toString(arr));
+    }
+}
